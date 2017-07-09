6ee1e77ad51a261a980bac89f282d6489728e046
Matt DeVoe <matthew.devoe@gmail.com>
2017-07-09 15:46:32 -0600

    Getting no method error for nil class, attempting to fix

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,17 @@
+require 'pry'
 class TagsController < ApplicationController
 
-  def show
-    @tag = Tag.find(params[:id])
-  end
 
   def index
     @tags = Tag.all
   end
 
+  def show
+    @tag = Tag.find(params[:id])
+  end
+  
   def destroy
+    binding.pry
     @tag.destroy
     flash.notice = "Tag '#{@tag.name}' deleted!"
     redirect_to tag_path
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,5 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :articles, through: :taggings
 
-  def to_s
-    name
-  end
+  
 end
